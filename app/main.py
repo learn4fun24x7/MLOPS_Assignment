ef24b8da124f98b7d0497d25f3f16ff2a2ed1bec
learn4fun24x7 <souravdas.personal.1@gmail.com>
2025-08-06 09:42:22 +0530

    MLOPS Assignment v.01

---
+++ app/main.py
@@ -0,0 +1,43 @@
+# app/main.py
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import mlflow.pyfunc
+import pandas as pd
+import logging
+
+# Setup logging
+logging.basicConfig(filename="logs/predictions.log", level=logging.INFO)
+
+# Define input schema
+class HousingInput(BaseModel):
+    longitude: float
+    latitude: float
+    housing_median_age: float
+    total_rooms: float
+    total_bedrooms: float
+    population: float
+    households: float
+    median_income: float
+
+# Load model from MLflow Model Registry
+model_name = "CaliforniaHousingModel"
+model_stage = "Staging"  # or "Production"
+# mlflow.set_tracking_uri("http://127.0.0.1:5000") # for Running this API in VM
+mlflow.set_tracking_uri("host.docker.internal:5000") # for Running this API in docker
+model = mlflow.pyfunc.load_model(model_uri=f"models:/{model_name}/{model_stage}")
+
+# Initialize FastAPI app
+app = FastAPI()
+
+@app.post("/predict")
+def predict(data: HousingInput):
+    input_dict = data.dict()
+    input_df = pd.DataFrame([input_dict])
+
+    prediction = model.predict(input_df)[0]
+
+    # Log request and prediction
+    logging.info(f"Input: {input_dict}, Prediction: {prediction}")
+
+    return {"prediction": prediction}
